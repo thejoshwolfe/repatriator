e97e2d0f1976aa3e7dc50d55236e6dd4538fb9de
Josh Wolfe <thejoshwolfe@gmail.com>
2010-09-30 00:08:46 -0700

    globalizing logging in client

--- client/ConnectionManager.cs
+++ client/ConnectionManager.cs
@@ -374,7 +374,6 @@ namespace repatriator_client
             public bool readMagicalResponse()
             {
                 byte typeCode = readByte();
-                Console.WriteLine("read byte");
                 if (typeCode != ResponseTypes.MagicalResponse)
                     return false;
                 long length = readLong();
@@ -449,14 +448,10 @@ namespace repatriator_client
         private class SocketStreamManager : StreamManager
         {
             private Socket socket;
-            private static bool logging = true;
-            private static string lastLoggingMode = null;
-            private static StreamWriter logFile = logging ? new StreamWriter("C:\\anaonetotototoot.txt", true) : null;
             public SocketStreamManager(Socket socket)
             {
                 this.socket = socket;
-                if (logging)
-                    logMessage("\ninit", "instance = " + this.GetHashCode() + ". ");
+                Logging.logMessage("\ninit", "instance = " + this.GetHashCode() + ". ");
             }
             public override void write(byte[] bytes)
             {
@@ -467,10 +462,10 @@ namespace repatriator_client
                     int amountWritten = socket.Send(bytes, index, amountToWrite, 0);
                     if (amountWritten == 0)
                     {
-                        logMessage("error", "can't send anything. ");
+                        Logging.logMessage("error", "can't send anything. ");
                         throw new SocketException();
                     }
-                    logCommunication("write", bytes, index, amountWritten);
+                    Logging.logCommunication("write", bytes, index, amountWritten);
                     index += amountWritten;
                     amountToWrite -= amountWritten;
                 }
@@ -486,42 +481,14 @@ namespace repatriator_client
                     int readSize = socket.Receive(buffer, offset, remainingSize, SocketFlags.None);
                     if (readSize == 0)
                     {
-                        logMessage("error", "can't read anything. ");
+                        Logging.logMessage("error", "can't read anything. ");
                         throw new SocketException();
                     }
-                    logCommunication("read", buffer, offset, readSize);
+                    Logging.logCommunication("read", buffer, offset, readSize);
                     offset += readSize;
                     remainingSize -= readSize;
                 }
                 return buffer;
-            }
-            private static void logCommunication(string header, byte[] bytes, int index, int length)
-            {
-                if (!logging)
-                    return;
-                string text = bytesToText(bytes, index, length);
-                logSomething(header, text);
-            }
-            private static void logMessage(string header, string message)
-            {
-                if (!logging)
-                    return;
-                logSomething(header, message);
-            }
-            private static void logSomething(string header, string text)
-            {
-                if (header != lastLoggingMode)
-                    logFile.Write("\n" + header + ": ");
-                lastLoggingMode = header;
-                logFile.Write(text);
-                logFile.Flush();
-            }
-            private static string bytesToText(byte[] bytes, int index, int length)
-            {
-                StringBuilder stringBuidler = new StringBuilder(bytes.Length * 3);
-                for (int i = index; i < index + length; i++)
-                    stringBuidler.Append(bytes[i].ToString("x2")).Append(' ');
-                return stringBuidler.ToString();
             }
         }
         private class FakeStreamWriter : StreamManager
---
+++ client/Logging.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace repatriator_client
+{
+    public static class Logging
+    {
+        private static bool logging = true;
+        private static string lastLoggingMode = null;
+        private static StreamWriter logFile;
+        private static object _lock = new object();
+        static Logging()
+        {
+            if (!logging)
+                return;
+            string path = "C:\\aoeu.txt";
+            logFile = new StreamWriter(path, true);
+        }
+        public static void logCommunication(string header, byte[] bytes, int index, int length)
+        {
+            if (!logging)
+                return;
+            string text = bytesToText(bytes, index, length);
+            logSomething(header, text);
+        }
+        public static void logMessage(string header, string message)
+        {
+            if (!logging)
+                return;
+            logSomething(header, message);
+        }
+        private static void logSomething(string header, string text)
+        {
+            lock (_lock)
+            {
+                if (header != lastLoggingMode)
+                {
+                    logFile.Write("\n" + header + ": ");
+                }
+                lastLoggingMode = header;
+                logFile.Write(text);
+                logFile.Flush();
+            }
+        }
+        private static string bytesToText(byte[] bytes, int index, int length)
+        {
+            StringBuilder stringBuidler = new StringBuilder(bytes.Length * 3);
+            for (int i = index; i < index + length; i++)
+                stringBuidler.Append(bytes[i].ToString("x2")).Append(' ');
+            return stringBuidler.ToString();
+        }
+    }
+}
