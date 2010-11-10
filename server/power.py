05000924165abf512efef5cc631fe1da52cad843
Andrew Kelley <superjoe30@gmail.com>
2010-11-10 01:06:28 -0700

    hooked up powerusb stuff

--- server/power.py
+++ server/power.py
@@ -1,10 +1,19 @@
 import logging
 from logging import debug, warning, error
+import powerusb
+import sys
+
+if powerusb.initialize():
+    debug("Initialized PowerUSB")
+else
+    error("Fatal: Unable to initialize PowerUSB")
+    sys.exit(-1)
 
 def set_power_switch(on):
     """
     if on is True, turns the power strip on.
     if on is False, turns the power strip off.
     """
-    status = {True: 'on', False: 'off'}
-    warning("(fake) turning {0} power strip".format(status[on]))
+    status = {True: 'on', False: 'off'}[on]
+    debug("turning {0} power strip".format(status))
+    powerusb.set_state(on, on)
