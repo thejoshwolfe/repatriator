9d3b1fa1e85c59611f7f0780d32624b2cf56a2ce
Josh Wolfe <thejoshwolfe@gmail.com>
2010-12-02 19:49:46 -0700

    ShadowSlider's thumb moves with mouse dragging.

--- client/ShadowSlider.cs
+++ client/ShadowSlider.cs
@@ -9,6 +9,7 @@ namespace repatriator_client
 {
     public class ShadowSlider : Control
     {
+        public event Action positionChosen;
         private int shadowPosition = 20;
         public int ShadowPosition
         {
@@ -63,9 +64,56 @@ namespace repatriator_client
             MaxPosition = 100;
         }
 
+        private Point mouseOrign;
+        private int positionOrigin;
+        private bool mouseIsDown = false;
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            mouseOrign.X = e.X;
+            mouseOrign.Y = e.Y;
+            positionOrigin = position;
+            mouseIsDown = true;
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (!mouseIsDown)
+                return;
+
+            if (orientation == System.Windows.Forms.Orientation.Vertical)
+            {
+                int pixelDelta = e.Y - mouseOrign.Y;
+                int positionDelta = (int)(pixelDelta / pixelsPerPositionY);
+                Position = positionOrigin + positionDelta;
+            }
+            else
+            {
+                // TODO
+            }
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            if (!mouseIsDown)
+                return;
+
+            mouseIsDown = false;
+            if (positionOrigin != position)
+                if (positionChosen != null)
+                    positionChosen();
+        }
+
+        private const int endMargin = 3;
+        private int trackHeight { get { return this.Height - endMargin * 2; } }
+        private float pixelsPerPositionY { get { return trackHeight / (float)maxPosition; } }
+        private int valueToY(int value)
+        {
+            return (int)(value * pixelsPerPositionY + endMargin);
+        }
         protected override void OnPaint(PaintEventArgs e)
         {
-            const int endMargin = 3;
             const int trackWidth = 4;
             const int thumbBreadth = 15;
             const int thumbSpan = 8;
@@ -77,17 +125,15 @@ namespace repatriator_client
             if (Orientation == System.Windows.Forms.Orientation.Vertical)
             {
                 int centerX = this.Width / 2;
-                int trackHeight = this.Height - endMargin * 2;
                 // track
                 g.FillRectangle(Brushes.LightGray, centerX - trackWidth / 2, endMargin, trackWidth, trackHeight);
 
-                float scale = trackHeight / (float)MaxPosition;
                 // shadow
-                int shadowCenterY = (int)(ShadowPosition * scale + endMargin);
+                int shadowCenterY = valueToY(ShadowPosition);
                 g.FillRectangle(Brushes.DarkGray, centerX - thumbBreadth / 2, shadowCenterY - thumbSpan / 2, thumbBreadth, thumbSpan);
 
                 // thumb
-                int thumbCenterY = (int)(Position * scale + endMargin);
+                int thumbCenterY =  valueToY(Position);
                 g.FillRectangle(Brushes.Black, centerX - thumbBreadth / 2, thumbCenterY - thumbSpan / 2, thumbBreadth, thumbSpan);
             }
             else
