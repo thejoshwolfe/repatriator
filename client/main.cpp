b046d2b419958be22d35ed638ec01fcf9a98d0b9
Andrew Kelley <superjoe30@gmail.com>
2011-01-09 20:56:06 -0700

    warn if we don't have jpeg plugin

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,8 @@
-#include <QtGui/QApplication>
+#include <QApplication>
+#include <QImageReader>
+#include <QMessageBox>
+#include <QDebug>
+
 #include "ConnectionWindow.h"
 #include "Settings.h"
 #include "MetaTypes.h"
@@ -6,6 +10,17 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // warn if can't find jpeg plugin
+    bool support_jpeg = QImageReader::supportedImageFormats().contains("jpg") ||
+                        QImageReader::supportedImageFormats().contains("jpeg");
+    Q_ASSERT(support_jpeg);
+    if (! support_jpeg) {
+        QMessageBox::warning(NULL, QObject::tr("imageformats/qjpeg4.dll not found"),
+            QObject::tr("Missing imageformats/qjpeg4.dll. You will not be able to see live view. Please reinstall this program."),
+           QMessageBox::Ok);
+    }
+
     MetaTypes::registerMetaTypes();
     Settings::load();
     ConnectionWindow::instance()->show();
